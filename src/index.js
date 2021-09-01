d47fd6dd9e8ecee7b5ec0ea76dbcf2fa653f2f47
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-08-31 22:11:38 -0300

    criação do middleware e post de usuarios

--- src/index.js
+++ src/index.js
@@ -1,21 +1,60 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
+const { json } = require('express');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
+
+// { 
+// 	id: 'uuid', // precisa ser um uuid
+// 	name: 'Danilo Vieira', 
+// 	username: 'danilo', 
+// 	todos: []
+// }
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
+  const { username } = request.header;
+
+  const existUser = users.find(x => x.username === username);
+
+  if(!existUser){
+    return response.status(404).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  
+  const { name, username } = request.body;
+
+  const existUsername = users.find(x => x.username === username);
+
+  if(existUsername){
+    return response.status(400).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
+  const user = {
+    id : uuidv4(),
+    name : name,
+    username : username,
+    todos : []
+  }
+
+  users.push(user)
+  
+  return response.status(201).json(user);
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
